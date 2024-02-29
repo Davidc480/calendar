4b277554807b69aeec257df7f0cc16438422c2c6
David <Davidcalle.480@gmail.com>
2024-02-29 17:29:14 -0600

    logic calculate days

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -10,41 +10,57 @@ import getDaysInMonth from '@/helper/getDaysInMonth';
 
 const Calendar = ()=>{
 
+    const [dateInfo, setDateInfo] = useState({
+        month: new Date().getMonth(),
+        year: new Date().getFullYear(),
+      });
+
     const calendarRef = useRef(null);
     const dateRef = useRef(null);
     const daysRef = useRef(null);
     const prevRef = useRef(null);
     const nextRef = useRef(null);
 
-    const [monthInfo, setmonthInfo] = useState("")
+    const [monthInfo, setMonthInfo] = useState("")
 
 
-    const refs = {
-        calendarRef,
-        dateRef,
-        daysRef,
-        prevRef,
-        nextRef,
-    };
+    const { weekdays, days, currentMonth, monthYear } = getCalendarData()
 
-    const { weekdays, days, currentMonth } = getCalendarData()
-
-    useEffect(()=>{
-        const dates = getDaysInMonth(refs)
-        setmonthInfo(dates)
-    }, [])
+    useEffect(() => {
+        const dates = getDaysInMonth(dateInfo.year, dateInfo.month);
+        setMonthInfo(dates);
+      }, [dateInfo]);
 
     // console.log(monthInfo.daysPrev);
     
-  
+    const handleClickprevMonth = () => {
+        setDateInfo(prev => ({
+          ...prev,
+          month: prev.month - 1 < 0 ? 11 : prev.month - 1,
+          year: prev.month - 1 < 0 ? prev.year - 1 : prev.year,
+        }));
+      };
+      
+      const handleClickNextMonth = () => {
+        setDateInfo(prev => ({
+          ...prev,
+          month: prev.month + 1 > 11 ? 0 : prev.month + 1,
+          year: prev.month + 1 > 11 ? prev.year + 1 : prev.year,
+        }));
+      };
+    
+      const handleClickToday = ()=>{
+        const dates = getDaysInMonth(dateInfo.year, dateInfo.month);
+        setMonthInfo(dates);
+      }
     return(
         <div id="container" className={`${styles.container} relative w-[1200px] min-h-[850px] mx-auto p-1 text-white flex rounded-xl bg-[#373c4f]`}>
             <div id="left" className="w-[60%] p-5">
                 <div id="calendar" ref={calendarRef} className={`${styles.calendar} relative w-full h-full flex flex-col flex-wrap justify-between text-[#878895] rounded-md bg-white`}>
                     <div id="month" className="w-full h-[150px] flex items-center justify-between px-[50px] text-base font-medium capitalize">
-                    <FontAwesomeIcon icon={faAngleLeft} ref={prevRef} className={`${styles.prev} h-7 w-7 cursor-pointer`} />
-                    <div id='date' ref={dateRef} className=''>{monthInfo.currentMonthAndYear}</div>
-                    <FontAwesomeIcon icon={faAngleRight} ref={nextRef} className={`${styles.next} h-7 w-7 cursor-pointer`} />
+                    <FontAwesomeIcon icon={faAngleLeft} ref={prevRef} onClick={handleClickprevMonth} className={`${styles.prev} h-7 w-7 cursor-pointer`} />
+                    <div id='date' ref={dateRef} className=''>{monthInfo.currentMonthAndYear }</div>
+                    <FontAwesomeIcon icon={faAngleRight} ref={nextRef} onClick={handleClickNextMonth} className={`${styles.next} h-7 w-7 cursor-pointer`} />
                     </div>
                     <div id="weekdays" className="w-full h-[100px] flex items-center justify-between px-5 font-medium capitalize">
                         {weekdays.map((weekday)=> 
@@ -59,7 +75,7 @@ const Calendar = ()=>{
                             <input type="text" placeholder='mm/yyyy' className='w-full h-[30px] outline-none border-none px-5 text-primaryColor rounded-md' />
                             <button id='goto-btn' className={`${styles.gotobtn} py-[5px] px-[10px] border border-primaryColor rounded-md bg-transparent cursor-pointer text-primaryColor `}>Go</button>
                         </div>
-                        <button id='today-btn' className={`${styles.todaybtn} py-[5px] px-[10px]  border border-primaryColor rounded-md bg-transparent cursor-pointer text-primaryColor`}>Today</button>
+                        <button id='today-btn' onClick={handleClickToday} className={`${styles.todaybtn} py-[5px] px-[10px]  border border-primaryColor rounded-md bg-transparent cursor-pointer text-primaryColor`}>Today</button>
                     </div>
                 </div>
             </div>
--- src/helper/getDaysInMonth.js
+++ src/helper/getDaysInMonth.js
@@ -1,27 +1,18 @@
 import getCalendarData from "./getCalendarData";
 
-const getDaysInMonth = ({
-  calendarRef,
-  dateRef,
-  daysRef,
-  prevRef,
-  nextRef,
-}) => {
-  let today = new Date();
+const getDaysInMonth = (year, month) => {
   let activeDay;
-  let month = today.getMonth();
-  let year = today.getFullYear();
 
   const { monthYear } = getCalendarData();
 
-  console.log(monthYear);
+  console.log(year);
 
   const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0);
   const prevLastDay = new Date(year, month, 0);
   const prevDays = prevLastDay.getDate();
   const lastDate = lastDay.getDate();
-  const day = firstDay.getDate();
+  const day = firstDay.getDay();
   const nextDays = 7 - lastDay.getDay() - 1;
 
   const currentMonthAndYear = monthYear[month] + " " + year;
@@ -30,30 +21,38 @@ const getDaysInMonth = ({
 
   for (let x = day; x > 0; x--) {
     prevDaysArray.push(
-      <div key={`prev-day-${x}`} className="prev-date">
+      <div key={`prev-date-${x}`} className="prev-date">
         {prevDays - x + 1}
       </div>
     );
   }
 
-  for (let i = 1; i < lastDate; i++) {
+  for (let i = 1; i <= lastDate; i++) {
     if (
       i === new Date().getDate() &&
       year === new Date().getFullYear() &&
       month === new Date().getMonth()
     ) {
       prevDaysArray.push(
-        <div key={`prev-day-${i}`} className="today-active">
+        <div key={`today-active-${i}`} className="today-active">
           {i}
         </div>
       );
     } else {
       prevDaysArray.push(
-        <div key={`prev-day-${i}`} className="day">
+        <div key={`day-${i}`} className="day">
           {i}
         </div>
       );
     }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    prevDaysArray.push(
+      <div key={`next-date-${j}`} className="next-date">
+        {j}
+      </div>
+    );
   }
 
   console.log(prevDaysArray);
@@ -61,6 +60,8 @@ const getDaysInMonth = ({
   const response = {
     currentMonthAndYear,
     daysPrev: prevDaysArray,
+    month,
+    year,
   };
 
   return response;
